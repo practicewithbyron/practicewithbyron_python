321bf10c6e2b89534536c3e8fd527a2a5dfe10dd
practicewithbyron <practicewithbyron@gmail.com>
2023-09-14 12:29:25 +0100

    Removed login function. Updated .env variable retrieval.

--- PracticeWithByronAPI-Python/__init__.py
+++ PracticeWithByronAPI-Python/__init__.py
@@ -70,8 +70,8 @@ app.add_middleware(
 
 load_dotenv()
 
-secret_key = os.environ("JWT_KEY")
-api_key = os.environ("API_KEY")
+secret_key = os.environ["JWT_KEY"]
+api_key = os.environ["API_KEY"]
 
 headers = {
     'Content-Type': 'application/json',
@@ -268,42 +268,38 @@ async def login(userAPIModel: UserAPIModel):
     :param userAPIModel: A model with a username and password
     :return: JWT with a payload including the users id, email, catalog, jwt exp and whether they're admin
     """
-    try:
-        if not IsTruthy(userAPIModel.email, userAPIModel.password):
-            raise HTTPException(
-                status_code=400, detail="Invalid email or password")
+    if not IsTruthy(userAPIModel.email, userAPIModel.password):
+        raise HTTPException(
+            status_code=400, detail="Invalid email or password")
 
-        payload = {
-            "email": userAPIModel.email
-        }
+    payload = {
+        "email": userAPIModel.email
+    }
 
-        response = requests.request(
-            "POST", "https://eu-west-2.aws.data.mongodb-api.com/app/data-vghcq/endpoint/api/readUser", headers=headers, params=payload)
+    response = requests.request(
+        "POST", "https://eu-west-2.aws.data.mongodb-api.com/app/data-vghcq/endpoint/api/readUser", headers=headers, params=payload)
 
-        if (response.text == "null"):
-            raise HTTPException(status_code=401, detail="User not found")
+    if (response.text == "null"):
+        raise HTTPException(status_code=401, detail="User not found")
 
-        hashed_password = bcrypt.hashpw(userAPIModel.password.encode(
-            'utf-8'), b'$2b$12$mCbAq1xSc21dn8o2Rj5Kou').decode("utf-8")
+    hashed_password = bcrypt.hashpw(userAPIModel.password.encode(
+        'utf-8'), b'$2b$12$mCbAq1xSc21dn8o2Rj5Kou').decode("utf-8")
 
-        json_response = json.loads(response.text)
+    json_response = json.loads(response.text)
 
-        if (json_response["password"] != hashed_password):
-            raise HTTPException(status_code=401, detail="Incorrect password")
+    if (json_response["password"] != hashed_password):
+        raise HTTPException(status_code=401, detail="Incorrect password")
 
-        # Create jwt
-        jwt = CreateJWT({"id": json_response["_id"], "email": userAPIModel.email, "catalog": json_response["catalog"],
-                        "exp": datetime.utcnow() + timedelta(days=1), "admin": json_response["admin"]})
+    # Create jwt
+    jwt = CreateJWT({"id": json_response["_id"], "email": userAPIModel.email, "catalog": json_response["catalog"],
+                    "exp": datetime.utcnow() + timedelta(days=1), "admin": json_response["admin"]})
 
-        if (hashed_password == json.loads(response.text)['password']):
-            raise HTTPException(
-                status_code=response.status_code, detail={"jwt": jwt})
-        else:
-            raise HTTPException(status_code=401, detail={
-                                'result': 'unauthorized'})
-    except:
-        raise HTTPException(status_code=500, detail={
-                            "result": "internal server error"})
+    if (hashed_password == json.loads(response.text)['password']):
+        raise HTTPException(
+            status_code=response.status_code, detail={"jwt": jwt})
+    else:
+        raise HTTPException(status_code=401, detail={
+                            'result': 'unauthorized'})
 
 
 @app.put("/api/updateUserCatalog")
